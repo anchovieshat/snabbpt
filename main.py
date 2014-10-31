da33375e00aca57eab57532ff6d321c8b793d5be
Colin Davidson <colrdavidson@gmail.com>
2014-10-31 18:38:57 -0400

    Handles directories properly

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from markdown import Markdown
 import sys
 import codecs
+import os
+import errno
 
-class Template:
+def ensure_output_exists(dir):
+    if not os.path.isdir(dir):
+        try:
+            print("mkdir", dir)
+            os.makedirs(dir)
+        except OSError as e:
+            raise SnabbptException("Unable to create output directory") from e
+
+
+class SnabbptException(Exception):
+    pass
+
+class HTMLTemplate:
     def __init__(self, html):
         self.html = html
+
     def from_file(filename):
         with codecs.open(filename, mode="r", encoding="utf-8") as file:
-            return Template(file.read())
-    def render(self, file):
-        with codecs.open(file.title + '.html', mode="w", encoding="utf-8", errors="xmlcharrefreplace") as out:
+            return HTMLTemplate(file.read())
+
+    def render(self, file, outfile):
+        with codecs.open(outfile, mode="w", encoding="utf-8", errors="xmlcharrefreplace") as out:
             out.write(self.html.replace("{{PAGE-TITLE}}", file.title).replace("{{PAGE-CONTENT}}", file.html))
 
 class File:
@@ -21,16 +37,68 @@ class File:
 
         self.md = Markdown(extensions=["markdown.extensions.meta"])
         self.html = self.md.convert(text)
-        self.title = self.md.Meta["title"][0]
-        self.template = self.md.Meta["template"][0]
+        self.title = self.md.Meta["title"][-1]
+        self.template = self.md.Meta["template"][-1]
+        self.output_path = "{0}/{1}.html".format(os.path.dirname(self.filename), self.title)
+
+    def __str__(self):
+        return self.filename
 
 class Renderer:
-    def __init__(self):
+    def __init__(self, outDir):
         self.templates = {}
+        self.outDir = outDir
+
+        ensure_output_exists(outDir)
+
+    def renderDir(self, path):
+        if not os.path.isdir(path):
+            raise SnabbptException("{0} is not a directory".format(path))
+        for file in self.get_files(path):
+            self.renderFile(file)
+
+    def get_files(self, path):
+        files = []
+        for file in os.listdir(path):
+            if file.startswith('.'):
+                continue
+            if os.path.isdir(file) and file != self.outDir:
+                files.extend(list(map(lambda x: os.path.normpath(os.path.join(path, file, x)), self.get_files(file))))
+            elif file.endswith(".md"):
+                files.append(file)
+        return files
+
     def renderFile(self, filename):
         file = File(filename)
         if file.template not in self.templates:
-            self.templates[file.template] = Template.from_file(file.template + '.html')
-        self.templates[file.template].render(file)
+            ext = file.template.split('.')[-1].upper()
+            if ext not in fileTypes:
+                raise SnabbptException("Invalid template type: {0}".format(ext))
+            self.templates[file.template] = fileTypes[ext].from_file(file.template)
 
-Renderer().renderFile(str(sys.argv[1]))
+        input_file = file.filename
+        output_file = os.path.join(self.outDir, file.output_path)
+        try:
+            if os.stat(input_file).st_mtime < os.stat(output_file).st_mtime:
+                print(file, "is up to date")
+                return
+        except:
+            pass
+
+        ensure_output_exists(os.path.dirname(output_file))
+
+        print("{0} -> {1}".format(input_file, output_file))
+        self.templates[file.template].render(file, output_file)
+
+fileTypes = {
+        "HTML":  HTMLTemplate,
+        }
+
+if __name__ == "__main__":
+    try:
+        Renderer(str(sys.argv[2])).renderDir(str(sys.argv[1]))
+    except SnabbptException as e:
+        if e.__cause__:
+            print("{0} ({1})".format(e, e.__cause__))
+        else:
+            print(e)
